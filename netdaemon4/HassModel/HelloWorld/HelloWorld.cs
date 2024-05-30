ba87dcd119c5cf3c2384bdea2c165ff67f5ce6d8
pharis\jlola <jlola@pharis.cz>
2024-05-30 19:23:48 +0200

    update added appdaemon4 pumpcontroller

---
+++ netdaemon4/HassModel/HelloWorld/HelloWorld.cs
@@ -0,0 +1,23 @@
+// Use unique namespaces for your apps if you going to share with others to avoid
+// conflicting names
+
+namespace HassModel;
+
+/// <summary>
+///     Hello world showcase using the new HassModel API
+/// </summary>
+[NetDaemonApp]
+public class HelloWorldApp
+{
+    public HelloWorldApp(IHaContext ha, ILogger<HelloWorldApp> logger)
+    {        
+        logger.LogTrace("This is at Verbose level");
+            logger.LogDebug("This is at Debug level");
+            logger.LogInformation("This is at Information level");
+            logger.LogWarning("This is at Warning level");
+            logger.LogError("This is at Error level");
+            logger.LogCritical("This is at Fatal level");
+
+        ha.CallService("notify", "persistent_notification", data: new {message = "Notify me", title = "Hello world!"});        
+    }
+}
